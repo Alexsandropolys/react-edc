2f44a1069845a49cfb53b924c5a088e5c04ee8d4
Alexander <alexsanderkarakulov@gmail.com>
2021-01-12 22:59:53 +0700

    First commit

---
+++ src/components/app/app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list'
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+import styled from 'styled-components';
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+const App = () => {
+
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'www'},
+        {label: 'That is so good', important: false, id: 'qqq'},
+        {label: 'I need a break...', important: false, id: 'eee'}
+    ];
+
+    return (
+    <AppBlock>
+        <AppHeader/>
+        <div className="search-panel d-flex">
+            <SearchPanel/>
+            <PostStatusFilter/>
+        </div>
+        <PostList posts={data}/>
+        <PostAddForm/>
+    </AppBlock>
+    )
+}
+
+export default App;
